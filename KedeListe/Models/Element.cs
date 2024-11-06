077a418305b8345fa83c13b50348f1e52953e0c1
Jacob Jørgensen <me@jacob-j.com>
2024-11-06 09:00:08 +0100

    start

---
+++ KedeListe/Models/Element.cs
@@ -0,0 +1,7 @@
+namespace KedeListe.Models;
+
+class Element
+{
+    public Object Data { get; set; }
+    public Element Next { get; set; }
+}
---
+++ KedeListe/Models/LinkedList.cs
@@ -0,0 +1,33 @@
+using KedeListe.Models;
+
+class LinkedList
+{
+    public Element First { get; set; }
+
+
+    public object? Add_First()
+    {
+        return null;
+    }
+    
+    public void Remove_First()
+    {
+        
+    }
+    
+    public int? count()
+    {
+        return null;
+    }
+
+    public string? To_String()
+    {
+        return null;
+    }
+
+    public void Sort()
+    {
+        
+    }
+}
+
